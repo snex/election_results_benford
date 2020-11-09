41da5ef72e179f6f26471729876cec6ed15bb223
David Havlicek <snex00@gmail.com>
2020-11-08 23:02:43 -0800

    remova commas from cook county numbers

--- il_cook.rb
+++ il_cook.rb
@@ -9,16 +9,16 @@ CSV.foreach('data/IL/cook_2016.csv', headers: true) do |row|
   precinct = row['Precinct']
   precincts[precinct] = {}
   precincts[precinct]['Trump'] = {
-    'total' => row['Donald J. Trump & Michael R. Pence']
+    'total' => row['Donald J. Trump & Michael R. Pence'].gsub(',', '')
   }
   precincts[precinct]['Clinton'] = {
-    'total' => row['Hillary R. Clinton & Timothy M. Kaine']
+    'total' => row['Hillary R. Clinton & Timothy M. Kaine'].gsub(',', '')
   }
   precincts[precinct]['Johnson'] = {
-    'total' => row['Gary Johnson & Bill Weld']
+    'total' => row['Gary Johnson & Bill Weld'].gsub(',', '')
   }
   precincts[precinct]['Stein'] = {
-    'total' => row['Jill Stein & Ajamu Baraka']
+    'total' => row['Jill Stein & Ajamu Baraka'].gsub(',', '')
   }
 end
 

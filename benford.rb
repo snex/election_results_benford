95b43494fce909795b0ed7fe4f5350276341a13e
David Havlicek <snex00@gmail.com>
2020-11-08 23:04:47 -0800

    i am reading that 2nd digit analysis is more important, so use that instead. also do a chi-square test on the output data

--- benford.rb
+++ benford.rb
@@ -1,11 +1,13 @@
 require 'amazing_print'
 
+BENFORD = [0.1197, 0.1139, 0.1088, 0.1043, 0.1003, 0.0967, 0.0934, 0.0904, 0.0876, 0.0850].freeze
+
 def benford precinct_data
   benford_data = {}
   precinct_data.each do |precinct, candidate_data|
     candidate_data.each do |candidate, vote_data|
       vote_data.each do |vote_type, count|
-        digit = count[0]
+        digit = count[1]
         next if digit.nil?
         benford_data[candidate] ||= {}
         benford_data[candidate][vote_type] ||= {
@@ -26,4 +28,25 @@ def benford precinct_data
   end
 
   ap benford_data
+  chi_square_test(benford_data)
+end
+
+def chi_square_test(benford_data)
+  benford_data.each do |candidate, c_data|
+    c_data.each do |vote_type, v_data|
+      total_count = v_data.map { |_, count| count }.sum
+      chi_square_stat = 0
+
+      v_data.each_with_index do |(digit, count), i|
+        expected_count = total_count * BENFORD[i]
+        chi_square = (count - expected_count) ** 2
+        chi_square_stat += chi_square / expected_count
+      end
+
+      puts ""
+      puts "Candidate: #{candidate}"
+      puts "Vote Type: #{vote_type}"
+      puts "Chi-squared Test Stat (over 15.51 is red flag): #{chi_square_stat}"
+    end
+  end
 end
